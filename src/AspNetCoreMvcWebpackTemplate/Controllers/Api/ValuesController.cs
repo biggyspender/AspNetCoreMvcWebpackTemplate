9454e533c24cdece75d30733f29da8d004da4d16
spender <christophermartinsperry@gmail.com>
2016-07-13 23:57:14 +0100

    basic javascript client added

--- src/AspNetCoreMvcWebpackTemplate/Controllers/Api/ValuesController.cs
+++ src/AspNetCoreMvcWebpackTemplate/Controllers/Api/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,7 @@ namespace AspNetCoreMvcWebpackTemplate.Controllers.Api
     public class ValuesController : Controller
     {
         // GET: api/values
+        [Authorize(Roles ="testrole")]
         [Route("api/Values")]
         [HttpGet]
         public IEnumerable<string> Get()
--- src/AspNetCoreMvcWebpackTemplate/Scripts/Src/main.js
+++ src/AspNetCoreMvcWebpackTemplate/Scripts/Src/main.js
@@ -1,1 +1,45 @@
-﻿console.log("hello world!");
+﻿require("babel-polyfill");
+var $ = require("jquery");
+
+var port= window.location.port &&
+    ((window.location.protocol.toLowerCase()=="http:"&&window.location.port!=80)
+    ||
+    (window.location.protocol.toLowerCase()=="https:"&&window.location.port!=443))
+?`:${window.location.port}`:"";
+    
+var origin = `${window.location.protocol}//${window.location.hostname}${port}`;
+
+var req=async ()=>{
+    var tokenData = await Promise.resolve($.ajax({
+        type: "POST",
+        url: "/connect/token",
+        data: {
+            "grant_type": "password",
+            username: "user@test.com",
+            password: "P2ssw0rd!",
+            scope: "profile email roles",
+            "resource": `${origin}/`
+        },
+        dataType: "json"
+    }));
+    var accessToken=tokenData.access_token;
+    var values = await Promise.resolve($.ajax({
+        type: "GET",
+        url: "/api/Values",
+        dataType: "json",
+        headers: { "Authorization": `Bearer ${accessToken}` }
+    }));
+    console.log(values);
+    var userinfo = await Promise.resolve($.ajax({
+        type: "GET",
+        url: "/connect/userinfo",
+        dataType: "json",
+        headers: { "Authorization": `Bearer ${accessToken}` }
+    }));
+    console.log(userinfo);
+};
+
+
+
+
+req();
